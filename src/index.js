d5c6b054f2c585ecd7afa58d64ab0501454905f3
Letícia De Araújo Nunes <leticiadearaujonunes@gmail.com>
2022-07-04 21:53:54 -0300

    feat: creating global style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { ThemeProvider } from "@mui/material/styles";
+
+import { GlobalStyle, theme } from "./styles/globalStyle";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
