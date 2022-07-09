57d5f431268c895ee7fb845e67e95d55d530eef3
Bruno <brunopassos_bp@hotmail.com>
2022-07-08 21:32:29 -0300

    fix/ fixed the text position at trips page

--- src/pages/Trips/style.js
+++ src/pages/Trips/style.js
@@ -3,7 +3,10 @@ import styled from "styled-components";
 export const StyledH1 = styled.h1`
     color: var(--blue);
     width: 100%;
-    margin-left: 1rem;
+    margin-left: 4rem;
+    margin-top: 1rem;
+    margin-bottom: 2rem;
+
     @media (min-width: 768px){
         margin-left: 6.5rem;
     }
