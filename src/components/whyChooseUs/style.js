c08be725bb8c5e0a3b7c2223a3f4ddd4cbeed86f
arc4dex <pedroiago.bernardes@gmail.com>
2022-07-13 19:47:14 -0300

    Fix: change 100vw to 100% in component wychooseUs

--- src/components/whyChooseUs/style.js
+++ src/components/whyChooseUs/style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ContainerWCU = styled.div`
-  width: 100vw;
+  width: 100%;
   height: fit-content;
   display: flex;
   flex-direction: column;
